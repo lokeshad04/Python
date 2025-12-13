10f02b3ff4e7def14b4d1e80d022aee88e56f755
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-13 15:15:27 +0530

    Create 05_fstrings.py

---
+++ 10_Strings/05_fstrings.py
@@ -0,0 +1,19 @@
+#String formatting
+
+template = "Dear {}, you are awesome. Take this {}$ bag"
+
+a = "John"
+a1 = 10000
+b = "Jack"
+b1 = 1000
+c = "Mary"
+c1 = 300
+
+# s1 = template.format(a,a1)
+# print(s1)
+
+print(f"Dear {a}, you are awesome. Take this {a1}$ bag")
+
+
+print(ord('A')) #Prints the ASCII value of A
+print(chr(65)) #gives the character in return of ASCII value 
