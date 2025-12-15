94f46e3449720245d70a23ed8f8b843b526a3461
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-15 18:02:50 +0530

    functions created

---
+++ 11_Functions/02_function_arguements.py
@@ -0,0 +1,6 @@
+def add(a, b):#a,b are parameters
+    x = a+b
+    return x
+
+c = add(3,5)#3,5 are arguements
+print(c)
---
+++ 11_Functions/03_lambda.py
@@ -0,0 +1,5 @@
+square = lambda x: x*x#it will take an input x and return x square
+print(square(2))
+sum = lambda x,y: x+y
+print(sum(4,23))
+
