e7cad6e1534e1dd4f1d4e0fa609d267c963f7e81
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-16 19:44:08 +0530

    functions updated

---
+++ 11_Functions/05_modules.py
@@ -0,0 +1,9 @@
+#Two types of functions: -Built in modules
+#                        -External modules
+import math #math is module name
+# import os
+# os.abort() 
+import _my_module
+print(math.sqrt(25))
+
+_my_module.hello()
---
+++ 11_Functions/_my_module.py
@@ -0,0 +1,4 @@
+def hello():
+    print("hello world".capitalize())
+
+#We will import this function to 05_modules.py
