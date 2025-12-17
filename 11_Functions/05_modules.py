f6e86c2712be491dd6e184f441be902c07f56496
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-17 13:43:08 +0530

    modules updated

--- 11_Functions/05_modules.py
+++ 11_Functions/05_modules.py
@@ -7,3 +7,13 @@ import _my_module
 print(math.sqrt(25))
 
 _my_module.hello()
+
+#now for external modules lets take requests modules for example
+#In terminal  pip install requests
+#  then we can do
+import requests
+r = requests.get("https://www.google.com")
+print(r.text)
+
+# pip install pandas
+import pandas as pd
