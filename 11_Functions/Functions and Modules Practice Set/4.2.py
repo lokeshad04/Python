39a3cfdeecc5e49f81953baf7179f4b8f4d2f9d4
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-19 23:22:31 +0530

    Update 4.2.py

--- 11_Functions/Functions and Modules Practice Set/4.2.py
+++ 11_Functions/Functions and Modules Practice Set/4.2.py
@@ -1,14 +1,24 @@
 # Write a recursive function sum_of_digits(n) that returns the sum of all digits
 # of a given number.
 
-sum = 0
 def sum_of_digits(n):
-    global sum
-    if(n<=0):
-        return sum
-    digit = n%10
-    
-    sum += digit
-    n //= 10
-    return sum_of_digits(n)
+    if n == 0:
+        return 0
+    return (n % 10) + sum_of_digits(n // 10)
+
 print(sum_of_digits(1234))
+
+
+# sum = 0
+# def sum_of_digits(n):
+#     global sum
+#     if(n<=0):
+#         return sum
+#     digit = n%10
+    
+#     sum += digit
+#     n //= 10
+#     return sum_of_digits(n)
+# print(sum_of_digits(1234))
+
+
