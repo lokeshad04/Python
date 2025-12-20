023fcf0fbd49df0d799a19865c7442a328e03791
lokeshad04 <lokeshgoswami004@gmail.com>
2025-12-20 16:29:23 +0530

    Create 7.1.py

---
+++ 11_Functions/Functions and Modules Practice Set/7.1.py
@@ -0,0 +1,26 @@
+# Write a recursive function fibonacci(n) that prints the first n Fibonacci
+# numbers
+
+# def fibonacci(n):
+#     '''
+#     Print the first n Fibonacci numbers using iteration.
+#     '''
+#     a, b = 0, 1   # First two Fibonacci numbers
+
+#     for i in range(n):
+#         print(a, end=" ")
+#         a, b = b, a + b
+
+
+def fibonacci(n):
+    def fib(k):
+        if(k<=1):
+            return k
+        return fib(k-1)+fib(k-2)
+
+    for i in range(n):
+        print(fib(i),end=" ")
+
+        
+        
+        
